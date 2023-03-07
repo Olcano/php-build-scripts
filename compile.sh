20ae54a72e30a452d9beac46dcb26771398e56f0
buchwasa <baddies.censers0o@icloud.com>
2023-03-06 21:01:00 -0500

    compile.sh: Add librdkafka support

--- compile.sh
+++ compile.sh
@@ -1118,6 +1118,7 @@ RANLIB=$RANLIB CFLAGS="$CFLAGS $FLAGS_LTO" CXXFLAGS="$CXXFLAGS $FLAGS_LTO" LDFLA
 --with-openssl \
 --with-zip \
 --with-libdeflate \
+--with-rdkafka="$INSTALL_DIR" \
 $HAS_LIBJPEG \
 $HAS_GD \
 --with-leveldb="$INSTALL_DIR" \
